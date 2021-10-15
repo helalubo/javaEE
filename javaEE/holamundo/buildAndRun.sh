ed6faff6aaa730cc72362389dcc18fb988814b2b
Helalubo <alejandrodemoraiz@gmail.com>
2021-10-15 01:25:17 -0300

    ok

--- javaEE/holamundo/buildAndRun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ $(docker ps -a -f name=holamundo | grep -w holamundo | wc -l) -eq 1 ]; then
-  docker rm -f holamundo
-fi
-mvn clean package && docker build -t com.helalubo/holamundo .
-docker run -d -p 9080:9080 -p 9443:9443 --name holamundo com.helalubo/holamundo
